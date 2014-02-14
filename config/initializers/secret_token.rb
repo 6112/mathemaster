eccedc33456715805d766536a0c81a0b9dce7f43
OpenShift guest <gotenks911@hotmail.com>
2014-02-13 21:48:35 -0500

    using rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mathemaster::Application.config.secret_key_base = '488786fce479443ba08492f8e480593fd92f2c456e369b48205d12b685dbb829b869b85c00fbedb180fad1643b9a62ba9c652a7a655cc664712c25324eb8a1c6'
