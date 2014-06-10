e02b756480ea72311760edb31b7175a276ab973b
minimus <ashlebede@gmail.com>
2014-06-09 21:40:17 -0400

    making sure Heroku serves the assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.serve_static_assets = true
+
 module Mathemaster
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
